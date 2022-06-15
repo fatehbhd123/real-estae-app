be59dffbf7cb1f53a8a4e9e0b1fd6683998d6d89
fatehbhd <bhdfateh2@gmail.com>
2022-06-15 12:03:11 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['bayut-production.s3.eu-central-1.amazonaws.com', 'q4g9y5a8.rocketcdn.me']
+  }
 }
 
 module.exports = nextConfig
